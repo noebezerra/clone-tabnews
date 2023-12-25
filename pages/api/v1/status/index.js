b49f860114250a6ab56cfa99e98e0fe6518939df
Noé Bezerra <noebezerra@hotmail.com>
2023-12-25 05:01:44 +0000

    finish `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,45 @@
 import database from "infra/database.js";
 
+async function databaseVersion() {
+  const response = await database.query("SHOW server_version;");
+  return response?.rows[0]?.server_version;
+}
+
+async function databaseMaxConnections() {
+  const response = await database.query("SHOW max_connections;");
+  const rows = response.rows[0].max_connections;
+  return rows;
+}
+
+async function databaseOpennedConnections() {
+  const databaseName = process.env.POSTGRES_DB;
+  const response = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const rows = response.rows[0].count;
+  return rows;
+}
+
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "status - on" });
+  const updatedAt = new Date().toISOString();
+  // Versão do postgres
+  const databaseVersionValue = await databaseVersion();
+  // Quantidade de conexões máximas
+  const databaseMaxConnectionsValue = await databaseMaxConnections();
+  // Conexões usadas
+  const databaseOpennedConnectionsValue = await databaseOpennedConnections();
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        oppened_connections: databaseOpennedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
