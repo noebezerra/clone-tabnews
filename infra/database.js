126ef3c45ebca1ddc1d449bcdbb4eccc5adc424f
Noé Bezerra <noebezerra@hotmail.com>
2024-04-06 21:07:35 +0000

    adds `/api/v1/migrations` endpoint

--- infra/database.js
+++ infra/database.js
@@ -1,6 +1,20 @@
 import { Client, Pool } from "pg";
 
 async function query(queryObject) {
+  let client;
+  try {
+    client = await getNewClient();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await client.end();
+  }
+}
+
+async function getNewClient() {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
@@ -13,20 +27,13 @@ async function query(queryObject) {
     ssl: getSSLValues(),
   });
 
-  try {
-    await client.connect();
-    const result = await client.query(queryObject);
-    return result;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  } finally {
-    await client.end();
-  }
+  await client.connect();
+  return client;
 }
 
 export default {
-  query: query,
+  query,
+  getNewClient,
 };
 
 function getSSLValues() {
@@ -34,5 +41,5 @@ function getSSLValues() {
     return { ca: process.env.POSTGRES_CA };
   }
 
-  return process.env.NODE_ENV === "development" ? false : true;
+  return process.env.NODE_ENV === "production" ? true : false;
 }
